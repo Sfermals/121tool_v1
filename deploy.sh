91cdc5c7ba1fa034c5ef955659e1235374386753
Sfermals <Azhar.sg@outlook.com>
2022-11-21 07:39:17 +0800

    fix container not running

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ DOCKER_IMAGE = sfermals/121tools:latest
 sudo su
 sudo docker pull $DOCKER_IMAGE
 sudo docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-sudo docker run -p 3000:3000 $DOCKER_IMAGE
+
 
 
 
